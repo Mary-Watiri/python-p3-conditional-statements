052d1a6becd1cb52b8b292e1a88098d93d41a4bb
Mary Watiri <watirimary30@gmail.com>
2024-03-01 23:55:31 +0300

    Completed Assignment

--- lib/control_flow.py
+++ lib/control_flow.py
@@ -1,17 +1,66 @@
 #!/usr/bin/env python3
 
-def admin_login(username, password):
-    # your code here
-    pass
 
-def hows_the_weather(temperature):
-    # your code here
-    pass
+   
+def admin_login(username, password):
+    if username.upper() == "ADMIN" and password == "12345":
+        return "Access granted"
+    else:
+        return "Access denied"
+print(admin_login("admin", "12345"))
+print(admin_login("ADMIN", "12345"))
+print(admin_login("adminn", "12345"))
+
+def hows_the_weather(temp):
+    if temp < 40:
+        return "It's brisk out there!"
+    elif 40 <= temp <= 65:
+        return "It's a little chilly out there!"
+    elif temp > 85:
+        return "It's too dang hot out there!"
+    else:
+        return "It's perfect out there!"
+
+hows_the_weather(55)
+hows_the_weather(99)
+hows_the_weather(75)
+hows_the_weather(33)
+
 
 def fizzbuzz(num):
-    # your code here
-    pass
+    if num % 3 == 0 and num % 5 == 0:
+        return "FizzBuzz"
+    elif num % 3 == 0:
+        return "Fizz"
+    elif num % 5 == 0:
+        return "Buzz"
+    else:
+        return num
 
-def calculator(operation, num1, num2):
-    # your code here
-    pass
+fizzbuzz(0)
+fizzbuzz(15)
+fizzbuzz(45)
+fizzbuzz(3)
+fizzbuzz(33)
+fizzbuzz(42)
+fizzbuzz(5)
+fizzbuzz(10)
+fizzbuzz(50)
+fizzbuzz(2)
+fizzbuzz(13)
+fizzbuzz(59)
+
+def calculator(operation, x, y):
+    dict_map = {
+        "+": x + y, 
+        "-": x - y,
+        "*": x * y,
+        "/": x / y,
+    }
+   
+    result = dict_map.get(operation)
+    if result is None:
+        print("Invalid operation!")
+        return None
+    else:
+        return result
